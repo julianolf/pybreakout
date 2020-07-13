f42e0871500e5c8bcf93014be7d3c931bd94f42a
Juliano Fernandes <julianofernandes@gmail.com>
2020-07-13 19:21:37 -0300

    Add initial structure

--- pybreakout/game.py
+++ pybreakout/game.py
@@ -1,19 +1,42 @@
 import pygame
 
+from . import settings
 
-def main():
-    pygame.init()
-    pygame.display.set_caption('PyBREAKOUT')
-    pygame.display.set_mode((480, 640))
 
-    running = True
-    while running:
+class Game:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption(settings.TITLE)
+        self.screen = pygame.display.set_mode(settings.WIN_SIZE)
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
-                break
+                self.running = False
+                return
 
-    pygame.quit()
+    def loop(self):
+        while self.running:
+            self.clock.tick(settings.FPS)
+            self.update()
+            self.draw()
+            self.events()
+
+    def run(self):
+        self.loop()
+        pygame.quit()
+
+
+def main():
+    Game().run()
 
 
 if __name__ == '__main__':
